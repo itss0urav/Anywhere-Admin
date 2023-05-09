75c034c0b6127f3717ac994edc00741208c7eee3
itss0urav <sourav2021official@gmail.com>
2023-05-09 21:04:59 +0530

    added delete

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -16,19 +16,21 @@ const Feedbacks = () => {
       <h1 className="text-2xl">Feedbacks from users</h1>
       <div className="h-1 w-full bg-gray-600 rounded-full mt-3"></div>
       <div className="mt-5">
-      {
-        feedbacks.map((feedback) => (
+        {feedbacks.map((feedback) => (
           <div className="border border-gray-700 px-4 py-3 rounded-md">
-          <h1>Username : {feedback?.username}</h1>
-          <h1>Email : {feedback?.email}</h1>
-          <p>Description : {feedback?.description}</p>
-          <p>Rating : {feedback?.rating}</p>
-        </div>
-        ))
-      }
+            <h1>Username : {feedback?.username}</h1>
+            <h1>Email : {feedback?.email}</h1>
+            <p>Description : {feedback?.description}</p>
+            <p>Rating : {feedback?.rating}</p>
+            <button
+              onClick={() => deleteUser(person._id)}
+              className="text-white bg-red-800 rounded px-3 py-1 hover:bg-red-500 "
+            >
+              Delete
+            </button>
+          </div>
+        ))}
       </div>
-      
-     
     </div>
   );
 };
